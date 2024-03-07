7d08ec880967f166a27807dbe588c1486efed79d
Jacob Edwards <91273401+jaxob333@users.noreply.github.com>
2024-03-07 13:10:06 -0600

    Add files via upload

---
+++ FizzBuzz_1.py
@@ -0,0 +1,9 @@
+for i in range(1, 101):
+    print(i)
+    
+    if i % 3 == 0:
+        print("Fizz")
+        if i % 5 == 0:
+            print("Buzz")
+    elif i % 5 == 0:
+        print("Buzz")
